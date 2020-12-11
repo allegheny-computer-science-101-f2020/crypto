3753838bfa855088fbb963b86119cde9836f627a
zackery <deversz@allegheny.edu>
2020-12-10 17:53:51 -1000

    fixing

--- src/main/java/project/RailFenceCipher.java
+++ src/main/java/project/RailFenceCipher.java
@@ -1,42 +1,40 @@
 package project;
 
-import java.util.*;
-/** railFenceCipher
+/** railFenceCipher.
  * @author zackery devers
  */
 
 public class RailFenceCipher {
-	int numRails;
+  int numRails;
 
-	public RailFenceCipher(int numRails) {
-		this.numRails = numRails;
-	}
-//call the encrypted data
-	String getEncryptedData(String data) {
-			char[] encrypted = new char[data.length()];
-			int n = 0;
+  public RailFenceCipher(int numRails) {
+    this.numRails = numRails;
+  }
+  //call the encrypted data
 
-			for(int k = 0 ; k < numRails; k ++) {
-				int index = k;
-				boolean down = true;
-				while(index < data.length() ) {
-					//System.out.println(k + " " + index+ " "+ n );
-					encrypted[n++] = data.charAt(index);
+  String getEncryptedData(String data) {
+    char[] encrypted = new char[data.length()];
+    int n1 = 0;
 
-					if(k == 0 || k == numRails - 1) {
-						index = index + 2 * (numRails - 1);
-					}
-					else if(down) {
-						index = index +  2 * (numRails - k - 1);
-						down = !down;
-					}
-					else {
-						index = index + 2 * k;
-						down = !down;
-					}
-				}
-			}
-			System.out.println(encrypted);
-			return new String(encrypted);
-		}
-	}
+    for (int k = 0 ; k < numRails; k ++) {
+      int index = k;
+      boolean down = true;
+      while (index < data.length() ) {
+        //System.out.println(k + " " + index+ " "+ n );
+        encrypted[n1++] = data.charAt(index);
+
+        if (k == 0 || k == numRails - 1) {
+          index = index + 2 * (numRails - 1);
+        } else if (down) {
+          index = index +  2 * (numRails - k - 1);
+          down = !down;
+        } else {
+          index = index + 2 * k;
+          down = !down;
+        }
+      }
+    }
+    System.out.println(encrypted);
+    return new String(encrypted);
+  }
+}
